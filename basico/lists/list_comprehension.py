7e05347817d66d940becbaa75745fd86050febde
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-08-25 03:52:56 -0300

    mais dois exemplos de list comprehension

--- basico/lists/list_comprehension.py
+++ basico/lists/list_comprehension.py
@@ -6,7 +6,12 @@ def second_list():
 	return [x + 1 if x % 2 == 0 else -1 for x in range(15)]
 
 
-# def third_list(iterable: Iterable[_T]):
+def third_list(iterable):
+	return [s for s in iterable if "a" in s]
+
+
+def fourth_list(iterable):
+	return [s for s in iterable if "a" not in s]
 
 
 if __name__ == '__main__':
@@ -14,4 +19,5 @@ if __name__ == '__main__':
 
 	print(first_list())
 	print(second_list())
-	# print(third_list())
+	print(third_list(fruits))
+	print(fourth_list(fruits))
